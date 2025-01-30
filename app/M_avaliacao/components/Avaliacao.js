4a725130c8b2576a90e78715d2dac09d5f8675df
theuz987 <mathamorim52@gmail.com>
2025-01-29 22:11:24 -0300

    base da pág. avaliacao criada

---
+++ app/M_avaliacao/components/Avaliacao.js
@@ -0,0 +1,16 @@
+export default function Avaliacao(){
+    return(
+        <div className="bg-white-100 flex justify-between p-4">
+            
+            <h1>SONUS</h1>
+            <div>
+                <a href="#">Início</a>
+                <a href="#">Login</a>
+                <a href="#">Cadastrar</a>
+                <a href="#">Avaliação</a>
+                <a href="#">Suporte</a>
+            </div>
+
+        </div>
+    );
+}
--- app/M_avaliacao/page.js
+++ app/M_avaliacao/page.js
@@ -1,23 +1,28 @@
 function Avaliacao() {
     return ( 
 
-    <div>
+    <div className="">
 
-        <div className="border-solid border w-1/6 bg-zinc-100 p-5">
-            <div className="text-center">
-                <img src="https://placehold.co/200"/>
-                </div>
-        </div>
         
-        <div class="ia">
-            <button><img src="https://placehold.co/200"/><p>Título da música</p> </button>
-            <br/>
-            <br/>
-            <label></label>
-        </div>
 
-        <div class="ia">
-            <p class="color">O que você achou da recomendação da música/do álbum?</p>
+        <br/>
+        <br/>  
+
+        <div className="border-solid border size-36">
+            <div>
+                <img src="https://placehold.co/200"/> 
+                <br/>
+                <br/>
+            </div>
+        </div>
+        <p>Título da música/do álbum</p>
+        
+        <br/>
+        <br/>
+        <br/>
+
+    <div>
+            <p>O que você achou da recomendação da música/do álbum?</p>
 
             <textarea id="story" name="story" rows="10" cols="53">
             </textarea>
