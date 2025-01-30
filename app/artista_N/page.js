e3b2d91f30be7fe917698fc6c1178f9f6ed5718a
Nicholla <nicollascl@hotmail.com>
2025-01-29 22:05:34 -0300

    Artista_N
    começei os Artistas

---
+++ app/artista_N/page.js
@@ -0,0 +1,12 @@
+import Cabecalho from "../albuns_N/components/Cabecalho";
+
+function Artista () {
+    return ( 
+        <div>
+        <Cabecalho/>
+
+        </div>
+     );
+}
+
+export default Artista ;
