9d7e7890a2642e643b99827dad462c6c563e1eda
Isaquezak64 <135741800+Isaquezak64@users.noreply.github.com>
2025-01-29 21:20:41 -0300

    pagina_principal ZAK

---
+++ app/components/Musicas.js
@@ -0,0 +1,24 @@
+function Musica(attr) {
+    return (  
+
+
+        <div className="border-solid border w-1/5 bg-zinc-50 p-5">
+
+            <div>
+            <img width ="100"  src = "../IMAGENS/bad.jpg"/> <p>Para Zak recomendamos Michael Jackson - Bad</p> 
+
+            <img width ="100"  src = "../IMAGENS/THE WEEKND logo.jpg"/> <p>Para Jhow recomendamos o artista THE WEEKND </p>
+
+            <img width ="100"  src = "../IMAGENS/BLACK SABBATH logo.png"/> <p>Para Matheus recomendamos o artista Black Sabbath</p>
+
+            <img width ="100"  src = "../IMAGENS/MAYHEM logo.jpg"/> <p>Para Nicolas recomendamos o artista Mayhem</p>
+
+
+            </div>
+         
+     
+        </div>
+    );
+}
+
+export default Musica;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,28 @@
-import Image from "next/image";
+import Musica from "./components/Musicas";
+import Noticias from "./components/Noticias";
+
 
 export default function Home() {
   return (
-    <div>
-      
+ <div>
+    
+<h1>Seus amigos vão gostar disso!!</h1>
+
+<div>
+
+<Musica/>
+
+</div>
+
+<div>
+
+  <Noticias/>
+  
+</div>
+
+
+
+
     </div>
   );
 }
