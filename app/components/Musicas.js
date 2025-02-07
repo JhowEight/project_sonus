5d5357154843e78c04d4843bfdcb0d7475143b30
Isaquezak64 <135741800+Isaquezak64@users.noreply.github.com>
2025-02-06 22:00:57 -0300

    subi a page atualizada
    page de verdade

--- app/components/Musicas.js
+++ app/components/Musicas.js
@@ -1,22 +1,22 @@
+import "./Musica.css"
+
 function Musica(attr) {
     return (  
 
 
-        <div className="border-solid border w-1/5 bg-zinc-50 p-5 ">
+        <div className="musica border-solid bg-purple-800 rounded-xl">
 
             <div>
-            <img src= "https://placehold.co/200"/>   <p>Para Zak recomendamos Michael Jackson - Bad</p> 
-
-            <img src= "https://placehold.co/200"/>   <p>Para Jhow recomendamos o artista THE WEEKND </p>
-
-            <img src= "https://placehold.co/200"/>   <p>Para Matheus recomendamos o artista Black Sabbath</p>
-
-            <img src= "https://placehold.co/200"/>   <p>Para Nicolas recomendamos o artista Mayhem</p>
-
-
-            </div>
+            <button> <img src={"imagens/"+attr.src}/>  </button>
+            <h3> {attr.nomemusica} <b/></h3>  
+            <p>{attr.comentario} </p> 
          
-     
+            
+            </div>    
+            <br/>
+            
+            
+
         </div>
     );
 }
--- app/components/Noticias.js
+++ app/components/Noticias.js
@@ -1,21 +1,27 @@
+
+
+
 function Noticias(attr) {
     return (  
-        <div>
+        <div >
             
-<h1>Noticias quea soam como musicas</h1>
+<h1 >Noticias quea soam como musicas</h1>
 
 <h2><li>APX 7 podcast voltado para o lado mais rock da coisa</li></h2>
 
-<p>O APX 7 Podcast é uma iniciativa que busca mergulhar de cabeça no universo das bandas de rock, com um olhar atento para as maiores e mais influentes do gênero. Através de episódios repletos de informações e discussões apaixonadas, os criadores do podcast proporcionam aos ouvintes uma experiência única e enriquecedora.</p>
+<p>O APX 7 Podcast é uma iniciativa que busca mergulhar de cabeça no universo das bandas de rock, com um olhar atento para as maiores e mais influentes do gênero.
+     Através de episódios repletos de informações e discussões apaixonadas, os criadores do podcast proporcionam aos ouvintes uma experiência única e enriquecedora.</p>
 
 <h2><li>Michael Jackson: Um Legado Eterno, Mesmo Ausente</li></h2>
 
-<p>Michael Jackson, o Rei do Pop, continua sendo uma figura icônica e influente na música mundial, mesmo após seu falecimento em 2009. Sua morte prematura, cercada de mistério e controvérsia, não apagou a luz de sua estrela, e seu legado permanece vivo e pulsante na indústria musical e na cultura popular.</p>
+<p>Michael Jackson, o Rei do Pop, continua sendo uma figura icônica e influente na música mundial, mesmo após seu falecimento em 2009.
+     Sua morte prematura, cercada de mistério e controvérsia, não apagou a luz de sua estrela, e seu legado permanece vivo e pulsante na indústria musical e na cultura popular.</p>
 
 <h2><li>Trapper Juice Wrld no game Fortnite</li></h2>
 
 <p>Juice WRLD em Fortnite: Uma Homenagem Inesperada
-Juice WRLD em Fortnite? Essa combinação pode parecer um tanto quanto inusitada à primeira vista, mas a Epic Games, criadora do popular battle royale, já demonstrou diversas vezes sua capacidade de surpreender os jogadores com crossovers improváveis.</p>
+Juice WRLD em Fortnite? Essa combinação pode parecer um tanto quanto inusitada à primeira vista, mas a Epic Games, 
+criadora do popular battle royale, já demonstrou diversas vezes sua capacidade de surpreender os jogadores com crossovers improváveis.</p>
 
        </div>
 
