44b3a70b37a146fb95c2870d3a10dc0588f0d23c
Isaquezak64 <135741800+Isaquezak64@users.noreply.github.com>
2025-02-06 22:33:07 -0300

    comitando state
    comit state

--- app/page.js
+++ app/page.js
@@ -1,6 +1,29 @@
 import Musica from "./components/Musicas";
 import Noticias from "./components/Noticias";
+import { useState } from "./components/Musicas";
 
+const ItemInfo = () =>{
+
+cont [ItemInfo, setItem] = useState({
+  nomeMusica:"BAD",
+  artista:"Michael Jackson"
+});
+
+return(
+  <div className="musica bg-zinc-80 gap-5 rounded-xl">
+
+
+
+<p>nomemusica:</p> {ItemInfo.nome}
+
+    </div>
+
+
+
+);
+
+
+};
 
 
 export default function Home() {
@@ -12,18 +35,18 @@ export default function Home() {
 <h1 >Seus amigos vão gostar disso!!</h1>
 
 <div className="musica bg-zinc-80 gap-5 rounded-xl">
-
+{/* 
 <Musica nomemusica="BAD" comentario=" Michael Jackson " src="bad.jpg"/> 
 <Musica nomemusica="aioaruno" comentario=" THE WEEKND" src="THE WEEKND.jpg"/>
 <Musica nomemusica="Heaven in hell" comentario="Black Sabbath" src="heaven in hell.jpg"/>
 <Musica nomemusica="siga e frente" comentario="Carreta Furacão"/>
 <Musica nomemusica="the scotts" comentario="Travis Scott" src="travis.jpg"/>
-<Musica nomemusica="Menino de rua" comentario="Pépe Moreno"/>
-
-<p>oi</p>
-
+<Musica nomemusica="Menino de rua" comentario="Pépe Moreno"/> */}
 
 </div>
+
+
+
 
 <br/>
 <hr/>
