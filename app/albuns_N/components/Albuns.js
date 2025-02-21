e147ee4614b3841f24093d13d1360fe621978128
Nicholla <nicollascl@hotmail.com>
2025-02-20 21:58:55 -0300

    lapidando a pagina albuns

--- app/albuns_N/components/Albuns.js
+++ app/albuns_N/components/Albuns.js
@@ -1,12 +1,12 @@
-function Albuns ( attr ) {
+function Musicas ( attr ) {
     return ( 
         <div className="flex w-60 pb-5">
     
-<div className=" border-solid border w-60 rounded-xl bg-violet-700 p-5">
+<div className=" border-solid border w-60 rounded-xl bg-[#362D58] p-5">
     <div className="text-center">
 <form action="">
     <button> 
-        <img src="https://placehold.co/200"/>
+        {/* <img src="https://placehold.co/200"/> */}
         <h2> <b>{attr.nome}</b> </h2>
     </button>
 </form>
