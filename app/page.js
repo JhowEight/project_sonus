4d7def4c682d93caab2392671b44517de14d7d84
Theuz987 <mathamorim52@gmail.com>
2025-05-22 19:40:20 -0300

    pequenas alterações feitas

--- app/page.js
+++ app/page.js
@@ -7,9 +7,7 @@ import Noticias from "./components/Noticias";
 import { useState } from "react";
 
 
-
 export default function Home() {
-
 
     // const [artistas, alteraArtistas]=useState([
 
@@ -29,11 +27,8 @@ export default function Home() {
        
     //     imagemArtista: ""
     //   }
-      
-      
 
     // ]);
-
 
     const [musicas, alteraMusicas] = useState([
         {
@@ -101,66 +96,47 @@ export default function Home() {
 
     return ( 
 
+      <div>
+        <h1 className="text-white text-center">Seus amigos vão gostar disso!!</h1>
+
         <div>
+          <ul className="musica bg-zinc-80 gap-5 rounded-xl overflow-x-auto">{   
+            musicas.map((i)=>
+            <Musica nomeMusica={i.nomeMusica} artista={i.artista} imagem={i.imagem}/>
+          )}
+          </ul>
+          <br/>                
+          <br/>
+        </div>
 
+        <h1 className="text-white text-center ">Albuns para você</h1>
+        <br/>
 
-            <h1 className="text-white text-center">Seus amigos vão gostar disso!!</h1>
+        <div className=" musica bg-zinc-80 gap-5 rounded-xl ">
 
-            <div >
-
-                <ul className="musica bg-zinc-80 gap-5 rounded-xl overflow-x-auto   ">{   
-                    musicas.map((i)=>
-                
-                    <Musica nomeMusica={i.nomeMusica} artista={i.artista} imagem={i.imagem}/> 
-                
-                )
-                    
-                    
-                    
-                }</ul>
-
-
-                <br/>                
-                <br/>
-                
-
-               </div>
-
-                <h1 className="text-white text-center ">Albuns para você</h1>
-                <br/>
-
-
-
-
-                <div className=" musica bg-zinc-80 gap-5 rounded-xl ">
-
-                  <a  href="./albuns_N"> <button className="rounded-xl bg-[#362D58] p-10 ">  <img src="/imagens/heaven in hell.jpg"/> </button></a>
+          <a  href="./albuns_N"> <button className="rounded-xl bg-[#362D58] p-10 ">  <img src="/imagens/heaven in hell.jpg"/> </button></a>
                   
-                  <button className="rounded-xl bg-[#362D58] p-10 "> <img src="/imagens/heaven in hell.jpg"/> </button>
+          <button className="rounded-xl bg-[#362D58] p-10 "> <img src="/imagens/heaven in hell.jpg"/> </button>
 
-                  <button className="rounded-xl bg-[#362D58] p-10 "> <img src="/imagens/heaven in hell.jpg"/> </button>
+          <button className="rounded-xl bg-[#362D58] p-10 "> <img src="/imagens/heaven in hell.jpg"/> </button>
 
-                  <button className="rounded-xl bg-[#362D58] p-10 "> <img src="/imagens/heaven in hell.jpg"/> </button>
+          <button className="rounded-xl bg-[#362D58] p-10 "> <img src="/imagens/heaven in hell.jpg"/> </button>  
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <hr/>
 
-                  
+        <h1 className="text-white text-center">Noticias que soam como musicas</h1>
 
-                </div>
-    <br/>
-    <br/>
-    <br/>
-    <hr/>
-    <h1 className="text-white text-center">Noticias que soam como musicas</h1>
+        <div className="text-white">
+          <Noticias/>
+        </div>
 
-         <div className="text-white">
+        <br/>
+        <hr/>
+        <br/>
 
-            <Noticias/>
-         </div>
-
-    <br/>
-    <hr/>
-    <br/>
-
-
-    </div>
+      </div>
     );
 }
