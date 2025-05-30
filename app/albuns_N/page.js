ee5fd37a6e3c9076b1b6f4df77a0b0a66b528df4
Theuz987 <mathamorim52@gmail.com>
2025-05-30 19:37:22 -0300

    conserto albuns

--- app/albuns_N/page.js
+++ app/albuns_N/page.js
@@ -41,7 +41,7 @@ function Album () {
             <h1 className="text-white text-3xl font-bold mb-4">{selecionaAlbum.nome}</h1>
 
             <div className="bg-[#362D58] max-w-7xl rounded-xl w-full flex flex-col md:flex-row text-white p-5">
-              <img className="rounded-xl w-full md:w-96 object-cover" src={selecionaAlbum.url_imagem} alt={selecionaAlbum.nome}/>
+              <img className="rounded-xl w-60 md:w-96 object-cover" src={selecionaAlbum.url_imagem} alt={selecionaAlbum.nome}/>
               <div className="p-4 md:ml-4 mt-4 md:mt-0 rounded-xl bg-[#1b2021] flex-1">
                 <p className="text-xl text-justify">
                   {selecionaAlbum.descricao}
